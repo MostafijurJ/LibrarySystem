330d1429faf96c0028c2cd29e11d24c98203b060
MostafijurJ <mrkajol550@gmail.com>
2020-11-25 12:51:22 +0600

    issue fixing

--- src/main/java/com/learn/springsecurity/ProfileManagementApplication.java
+++ src/main/java/com/learn/springsecurity/ProfileManagementApplication.java
@@ -1,11 +1,5 @@
 package com.learn.springsecurity;
 
-import com.learn.springsecurity.entities.Book;
-import com.learn.springsecurity.entities.User;
-import com.learn.springsecurity.repository.BookRepository;
-import com.learn.springsecurity.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
--- src/main/java/com/learn/springsecurity/controller/usercontroller/UserRegistrationController.java
+++ src/main/java/com/learn/springsecurity/controller/usercontroller/UserRegistrationController.java
@@ -5,13 +5,13 @@ import com.learn.springsecurity.dto.UserRegistrationDto;
 import com.learn.springsecurity.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import javax.validation.Valid;
 
 
