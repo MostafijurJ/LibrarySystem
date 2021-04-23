3a7ea40eab463840c0c21a1d64a98a19998f5e2a
mostafijurr <Mostafijur.Rahman@brainstation23.com>
2021-04-23 18:58:37 +0600

    refactored

--- src/main/java/com/learn/springsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/learn/springsecurity/config/SecurityConfiguration.java
@@ -17,57 +17,57 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UserService userService;
+  @Autowired
+  private UserService userService;
 
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-        auth.setUserDetailsService(userService);
-        auth.setPasswordEncoder(passwordEncoder());
-        return auth;
-    }
+  @Bean
+  public DaoAuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+    auth.setUserDetailsService(userService);
+    auth.setPasswordEncoder(passwordEncoder());
+    return auth;
+  }
 
-    @Bean
-    public static NoOpPasswordEncoder passwordEncoder() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-    }
+  @Bean
+  public static NoOpPasswordEncoder passwordEncoder() {
+    return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+  }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
-    }
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) {
+    auth.authenticationProvider(authenticationProvider());
+  }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers(
-                "/registration**",
-                "/js/**",
-                "/css/**",
-                "/img/**").permitAll()
-                .antMatchers("/addBook/**").hasRole("ADMIN")
-                .antMatchers("/home/**").hasAnyRole("USER","ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login")
-                .permitAll();
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().antMatchers(
+        "/registration**",
+        "/js/**",
+        "/css/**",
+        "/img/**").permitAll()
+        .antMatchers("/addBook/**").hasRole("ADMIN")
+        .antMatchers("/home/**").hasAnyRole("USER", "ADMIN")
+        .anyRequest().authenticated()
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .permitAll()
+        .and()
+        .logout()
+        .invalidateHttpSession(true)
+        .clearAuthentication(true)
+        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+        .logoutSuccessUrl("/login")
+        .permitAll();
 
 
-        // Role wise restrictions
+    //TODO Role wise restrictions
 		/*http.authorizeRequests()
 				.antMatchers("/admin").hasRole("ADMIN")
 				.antMatchers("/user").hasAnyRole("USER", "ADMIN")
 				.antMatchers("/").permitAll()
 				.and().formLogin();*/
 
-    }
+  }
 
 }
