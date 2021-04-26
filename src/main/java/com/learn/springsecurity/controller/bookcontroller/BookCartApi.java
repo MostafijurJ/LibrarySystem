bf59df871acd0ce538890ee3c1ddaf22aec5a502
mostafijurr <Mostafijur.Rahman@brainstation23.com>
2021-04-26 16:33:23 +0600

    api interfaces added in book carted

---
+++ src/main/java/com/learn/springsecurity/controller/bookcontroller/BookCartApi.java
@@ -0,0 +1,4 @@
+package com.learn.springsecurity.controller.bookcontroller;
+
+public interface BookCartApi {
+}
--- src/main/java/com/learn/springsecurity/controller/bookcontroller/BookCartController.java
+++ src/main/java/com/learn/springsecurity/controller/bookcontroller/BookCartController.java
@@ -1,24 +1,17 @@
-/*
-  Author: Mostafijur Rahman
-  Date: 11/11/2020
-  Time: 5:10 PM
-*/
-
-
 package com.learn.springsecurity.controller.bookcontroller;
 
 import com.learn.springsecurity.entities.Book;
 import com.learn.springsecurity.service.BookCartService;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.util.List;
 
-@Controller
-public class BookCartController {
+@RestController
+public class BookCartController implements BookCartApi {
 
   private final BookCartService bookCartService;
 
